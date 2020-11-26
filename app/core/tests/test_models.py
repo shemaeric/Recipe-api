51d617b6ce464bdf50e093d957d21c6e4a090881
shemaeric <shemaeric01@gmail.com>
2020-11-26 16:53:31 +0200

    add the functionalities for super user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,24 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normilized(self):
+        """Test the email for new user is normalized"""
+        email = 'test@SNOW.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating a new user with no email raises an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_if_super_user_created(self):
+        """Test creating new user"""
+        user = get_user_model().objects.create_superuser(
+            'test@snowq.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
